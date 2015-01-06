175db95a2a86bd87b7ba842e9e587bf3cce952ca
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2015-01-06 20:27:44 +0300

    Show action for trapped requests

--- app/controllers/trapped_requests_controller.rb
+++ app/controllers/trapped_requests_controller.rb
@@ -1,5 +1,6 @@
 class TrappedRequestsController < ApplicationController
   before_action :set_trap_id
+  skip_before_filter :verify_authenticity_token
 
   def index
     @trapped_requests = TrappedRequest.for_trap(@trap_id)
@@ -8,12 +9,12 @@ class TrappedRequestsController < ApplicationController
   def create
     TrappedRequest.create!(
       trap_id: @trap_id,
-      params: params.to_json,
+      params: serialize(simple_values(params)),
       referrer: request.referer,
       http_method: request.method,
       remote_ip: request.remote_ip,
       scheme: request.scheme,
-      headers: Hash[request.headers.to_a],
+      headers: serialize(simple_values(request.headers)),
     )
 
     head :ok
@@ -22,10 +23,34 @@ class TrappedRequestsController < ApplicationController
   end
 
   def show
+    @trapped_request = TrappedRequest.for_trap(@trap_id).with_id(params[:id]).first
+
+    unless @trapped_request
+      render file: 'public/404.html', status: :not_found, layout: false
+    end
   end
 
   private
   def set_trap_id
     @trap_id = params.fetch(:trap_id)
+  end
+
+  def serialize(hash)
+    JSON.generate(hash)
+  end
+
+  def simple_values(key_value_list)
+    key_value_list.each_with_object({}) do |(k, v), memo|
+      memo[k] = v if simple_value?(v)
+    end
+  end
+
+  def simple_value?(v)
+    case v
+    when String, Fixnum, Hash, Array, TrueClass, FalseClass, NilClass
+      true
+    else
+      false
+    end
   end
 end
--- app/models/trapped_request.rb
+++ app/models/trapped_request.rb
@@ -1,4 +1,17 @@
 class TrappedRequest < ActiveRecord::Base
 
   scope :for_trap, ->(trap_id) { where(trap_id: trap_id) }
+  scope :with_id, ->(id) { where(id: id) }
+
+  def params
+    JSON.parse(read_attribute('params'))
+  rescue
+    nil
+  end
+
+  def headers
+    JSON.parse(read_attribute('headers'))
+  rescue
+    nil
+  end
 end
